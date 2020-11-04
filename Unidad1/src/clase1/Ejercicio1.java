00443f930d18db27b07e6a54622dc1f242ea4b1f
Moises_Mac <moises.cambre.perez@gmail.com>
2020-11-04 21:13:19 +0100

    primer paso

--- Unidad1/src/clase1/Ejercicio1.java
+++ Unidad1/src/clase1/Ejercicio1.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicio1;
+package clase1;
 
 /**
  *
  * @author moises
  */
-public class Clase1 {
+public class Ejercicio1 {
 
     /**
      * @param args the command line arguments
