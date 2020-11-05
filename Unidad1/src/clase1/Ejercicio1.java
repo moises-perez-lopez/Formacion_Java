4e58e99ee1d32fba9cccd21001711fc2e1656b2c
Moises_Mac <moises.cambre.perez@gmail.com>
2020-11-05 19:08:31 +0100

    Unidad 1, clase 1, ejercicio 1

--- Unidad1/src/clase1/Ejercicio1.java
+++ Unidad1/src/clase1/Ejercicio1.java
@@ -5,17 +5,31 @@
  */
 package clase1;
 
-/**
- *
- * @author moises
- */
+/* 
+        1º Vamos a crear un metodo que reciba como datos de entrada dos numeros y devuelva el resultado de la suma
+        inputs : 2 numeros
+        output : suma
+        Para LEER datos de teclado : (new java.util.Scanner(System.in)).METODO_PARA_CADA_TIPO_DE_DATOS;
+
+*/
 public class Ejercicio1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        double a, b;
+        System.out.println("Introduzca el primer sumando");
+        a = (new java.util.Scanner(System.in)).nextDouble();
+        System.out.println("Introduzca el segundo sumando");
+        b = (new java.util.Scanner(System.in)).nextDouble();
+        System.out.println("El resultado es: " + suma(a,b));
+    }
+    
+    public static double suma(double a, double b){
+      double resultado;  
+      resultado = a + b;
+      return resultado;
     }
     
 }
