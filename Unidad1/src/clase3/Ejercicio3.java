0c0fd1560a7fcfb4bae31f89e81d8e65f0b485bc
Moises_Mac <moises.cambre.perez@gmail.com>
2020-11-05 21:20:04 +0100

    Unidad 1 Constructores y Arrays, clase 3, ejercicio 3

---
+++ Unidad1/src/clase3/Ejercicio3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3;
+
+/*
+
+    En cada clase, se pueden crear los constructores que queramos.
+
+    Vamos a crear un nuevo constructor en la clase Movimiento para pasarle los 3 datos (fecha, tipo y cantidad) mediante el constructor.
+
+    El número de constructores y el número de argumentos dependerá de los requerimientos que nos pidan
+
+*/
+
+public class Ejercicio3 {
+    
+    public static void main(String[] args){
+        
+        CuentaBancaria_ej2 cuenta = new CuentaBancaria_ej2(3);
+        
+    }
+}
---
+++ Unidad1/src/clase3/Movimiento_ej3.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3;
+
+import java.util.Date;
+
+/**
+ *
+ * @author moises
+ */
+public class Movimiento_ej3 {
+    private Date fecha;
+    private String operacion;
+    private Double cantidad;
+    private Double saldo;
+
+    public Movimiento_ej3(Date fecha, String operacion, Double cantidad) {
+        this.fecha = fecha;
+        this.operacion = operacion;
+        this.cantidad = cantidad;
+    }
+    
+    public Movimiento_ej3(){
+        
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = operacion;
+    }
+
+    public Double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
